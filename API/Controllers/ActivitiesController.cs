9ec64d6224c644da8f392823e1103cec24557b50
Kelwin <kelwinarthur8@gmail.com>
2021-07-27 14:10:37 -0300

    Remove cached bin/obj

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -14,6 +14,8 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
 
+
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetActivities()
         {
